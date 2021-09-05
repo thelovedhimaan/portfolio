cc744ab61de0f808467d46319a0cf7f8c523e32e
Love Dhimaan <thelovedhimaan@gamil.com>
2021-09-05 22:50:23 +0530

    company changes

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -28,23 +28,23 @@ const ExperienceCard = ({data}) => {
     }
 
     return ( 
-        <Col lg="6">
+        <Col lg="12">
         <Fade left duration={1000} distance="40px">
             <Card style={{flex: 1}} className="shadow-lg--hover shadow border-0 text-center rounded">
                 <CardHeader style={{background: '#14123B'}} >
-                    <h5 className="text-white">{data.company}</h5>
+                    <h3 className="text-white">{data.company}</h3>
                 </CardHeader>
-                <CardBody className="py-5">
-                    <img ref={imgRef} className=" bg-white rounded-circle mb-3 img-center img-fluid shadow-lg " src={data.companylogo} style={{ width: "100px" }} onLoad={() => getColorArrays()} alt=""/>
-                    <CardTitle tag="h5">{data.role}</CardTitle>
+                <CardBody >
+                    <img ref={imgRef} className=" bg-white rounded-circle  img-center img-fluid shadow-lg " src={data.companylogo} style={{ width: "100px" }} onLoad={() => getColorArrays()} alt=""/>
+                    <CardTitle tag="h4">{data.role}</CardTitle>
                     <CardSubtitle>{data.date}</CardSubtitle>
-                    <CardText className="description my-3 text-left">
-                        {data.desc}
-                        <ul>
+                    <CardText className="description my-3  text-left" style={{ fontWeight:'500'}}>
+                        <div className='mb-2'>{data.desc}</div>
+                        <ul >
                             {
                                 data.descBullets ? 
                                 data.descBullets.map((desc) => {
-                                    return <li key={desc}>{desc}</li>
+                                    return <li className='mb-2' key={desc}>{desc}</li>
                                 }) : null
                             }
                         </ul>
